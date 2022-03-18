2e2166c7596d79c36bc2c12e68e9668e536aa874
Scott <99330150+scottyamada@users.noreply.github.com>
2022-03-18 21:45:52 +0000

    Added lock screen functionality
    Screen seems to unlock no matter what is entered, fine for demo purposes.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LockScreen from "./screens/LockScreen";
 import Home from "./screens/Home";
 import Files from "./screens/Files";
 import Prop from "./screens/Prop";
@@ -9,40 +10,24 @@ import Something from "./screens/Something";
 import Upload from "./screens/Upload";
 import Vault from "./screens/Vault";
 import Example from "./screens/Example";
-import React from "react";
-import { Button, SafeAreaView, Alert } from "react-native";
-import { storage } from "./firebase";
+import React, { useEffect } from "react";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
-
-  const formHandler = (e) => {
-    e.preventDefault();
-    const file = e.target[0].files[0];
-    uploadFiles(file);
-  };
-
-  const uploadFiles = (file) => {
-    //
-    if (!file) return;
-    const sotrageRef = ref(storage, `files/${file.name}`);
-    const uploadTask = uploadBytesResumable(sotrageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {},
-      (error) => console.log(error),
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-        });
-      }
-    );
-  };
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          component={LockScreen}
+          name="lock"
+          options={{
+            title: "Input PIN:",
+            headerStyle: {
+              backgroundColor: "#131313",
+            },
+            headerTintColor: "#ffffff",
+          }}
+        />
         <Stack.Screen
           component={Home}
           name="home"
--- screens/Upload.js
+++ screens/Upload.js
@@ -1,69 +1,10 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-  Button,
-} from "react-native";
-import React, { useEffect, useState } from "react";
-import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
-import * as Firebase from "../firebase";
-import * as DocumentPicker from "expo-document-picker";
-import * as ImagePicker from "expo-image-picker";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Upload = () => {
-  const [image, setImage] = useState(null);
-
-  const pickDocument = async () => {
-    let result = await DocumentPicker.getDocumentAsync({});
-    alert(result.uri);
-    console.log(result);
-  };
-
-  const formHandler = (e) => {
-    e.preventDefault();
-    const file = e.target[0].files[0];
-    uploadFiles(file);
-  };
-
-  const uploadFiles = (file) => {
-    //
-    if (!file) return;
-    const sotrageRef = ref(storage, `files/${file.name}`);
-    const uploadTask = uploadBytesResumable(sotrageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {},
-      (error) => console.log(error),
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-        });
-      }
-    );
-  };
-
   return (
-    <View style={styles.container}>
-      <View style={styles.btnContainer}>
-        <View>
-          <TouchableOpacity onPress={uploadFiles}>
-            <View style={styles.btn}>
-              <Text>Upload</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-
-        <View>
-          <TouchableOpacity onPress={pickDocument}>
-            <View style={styles.btn}>
-              <Text>Choose File</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
+    <View>
+      <Text>Upload</Text>
     </View>
   );
 };
@@ -72,19 +13,8 @@ export default Upload;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignContent: "center",
-    backgroundColor: "#131313",
     justifyContent: "center",
-  },
-  btn: {
-    backgroundColor: "#AC4BBF",
-    padding: 12,
-  },
-  btnContainer: {
-    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
+    flex: 1,
   },
 });
